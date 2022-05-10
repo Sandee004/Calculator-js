59a293c22e0c8d1ae9fac6384fdd405b6af05651
Sandee Tee <102047643+Sandee004@users.noreply.github.com>
2022-05-10 16:46:10 +0100

    Add files via upload

---
+++ calculator 2.js
@@ -0,0 +1,43 @@
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let screen = document.querySelector("#screen");
+let clearBtn = document.querySelector('#clear');
+
+let total = document.querySelector("#total");
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+
+// For Number Inputs
+btnSpread.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    // Inner Values for calculator
+
+    if (screen.innerHTML == "0") {
+      screen.innerHTML = "";
+    }
+
+    let value = btns[i].innerHTML;
+    screen.innerHTML += value;
+  });
+});
+
+
+// Function to evalute Strings
+function evaluate(fn) {
+    return new Function('return ' + fn)();
+}
+
+// To calculate All Input
+total.addEventListener('click', ()=> {
+let allInputs = screen.innerHTML;
+
+screen.innerHTML = evaluate(allInputs);
+
+console.log(evaluate(allInputs));
+})
+
+// Clear all Inputs
+clearBtn.addEventListener('click', ()=> {
+    screen.innerHTML = "0";
+})
